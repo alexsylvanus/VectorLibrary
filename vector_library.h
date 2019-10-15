70ac540b323d8bb4aed75e4ac6e7e362afc254c4
alexsylvanus <alexsylvanus@gmail.com>
2019-10-15 18:50:43 -0400

    Add library header file

---
+++ vector_library.h
@@ -0,0 +1,15 @@
+/* File: vector_library.h
+ * Author: Alex Sylvanus
+ * Description: Includes all files vector library functions are dependant on.
+ * 
+ */
+
+/* Begin Header */
+#ifndef VECTOR_LIBRARY_H_
+#define VECTOR_LIBRARY_H_
+
+/* Includes */
+#include "inc/avector.h"
+#include "inc/astring.h"
+
+#endif /* VECTOR_LIBRARY_H_ */
