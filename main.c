1314abd646b942b83b111f5376eb4fe1e8478ecf
Alex Sylvanus <alex.sylvanus@ansys.com>
2020-10-29 18:06:38 -0400

    Add multi-clear test at the end of main

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 #include "adate.h"
 #include "vectorTest.h"
 #include "export.h"
+#include "avector.h"
 
 // Variable Type Definitions
 
@@ -38,6 +39,26 @@ int main() {
 	printf("\n");
 
 	TestAll();
+	string_t str = sInit("");
+	clear(&str);
+
+	printf("\nTesting initialize and clear empty vectors.\n");
+	vint_t vector_int = vInitI(NULL, 0);
+	cleari(vector_int);
+	cleari(vector_int);
+
+	vfloat_t vector_float = vInitF(NULL, 0);
+	clearf(vector_float);
+	clearf(vector_float);
+
+	vdouble_t vector_double = vInitD(NULL, 0);
+	cleard(vector_double);
+
+	vstring_t vector_string = vInitS_Blank();
+	clears(vector_string);
+	clears(vector_string);
+
+	printf("\nSuccess! Clear can be called multiple times on the same vector. \n");
 	return 0;
 }
 
